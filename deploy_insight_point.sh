2127716f566f81a97f1186f96310d4f5dc1d7620
tl-veza <106997411+tl-veza@users.noreply.github.com>
2022-12-07 07:58:24 -0800

    Update deploy_insight_point.sh

--- deploy_insight_point.sh
+++ deploy_insight_point.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
-ecr_registry=public.ecr.aws/b1u0k2e1/insight_point:latest
 docker run --detach \
    --restart=always \
    --log-driver local \
    --log-opt max-size=100m \
-   -e COOKIEAI_CP_ADDR="$VEZA_FQDN:443" \
-   -e DP_REGISTER_KEY="$VEZA_IP_REGISTER_KEY" \
-   $ecr_registry
+   --log-opt max-file=5 \
+   --env COOKIEAI_CP_ADDR="$VEZA_FQDN:443" \
+   --env DP_REGISTER_KEY="$VEZA_IP_REGISTER_KEY" \
+   --name insight_point \
+   public.ecr.aws/b1u0k2e1/insight_point:latest
